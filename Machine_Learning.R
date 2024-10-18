a230e844b079b4051035b27b11a12427fa1e6e9f
Patrick Niekamp <pniekamp113@gmail.com>
2024-10-18 16:39:48 -0400

    initiate

---
+++ Machine_Learning.R
@@ -0,0 +1,56 @@
+#Maschine Learning
+#Module 1
+
+library(kernlab)
+data(spam)
+head(spam) 
+
+plot(density(spam$your[spam$type=="nonspam"]),
+     col="blue",main="",xlab="Frequency of 'your'")
+lines(density(spam$your[spam$type=="spam"]),col="red")
+
+
+#predict SPAM based on frequency of "your"
+
+plot(density(spam$your[spam$type=="nonspam"]),
+     col="blue",main="",xlab="Frequency of 'your'")
+lines(density(spam$your[spam$type=="spam"]),col="red")
+abline(v=0.5,col="black")
+
+prediction <- ifelse(spam$your > 0.5,"spam","nonspam")
+table(prediction,spam$type)/length(spam$type)
+
+#Output shows that the algorithm is about 75% accurate -> 45% for nonspam + 30% spam
+
+
+
+# In sample vs out of sample errors
+# Spam messages based on capital letters
+set.seed(333)
+smallSpam <- spam[sample(dim(spam)[1],size=10),]
+spamLabel <- (smallSpam$type=="spam")*1 + 1
+plot(smallSpam$capitalAve,col=spamLabel)
+
+rule1 <- function(x){
+  prediction <- rep(NA,length(x))
+  prediction[x > 2.7] <- "spam"
+  prediction[x < 2.40] <- "nonspam"
+  prediction[(x >= 2.40 & x <= 2.45)] <- "spam"
+  prediction[(x > 2.45 & x <= 2.70)] <- "nonspam"
+  return(prediction)
+}
+table(rule1(smallSpam$capitalAve),smallSpam$type)
+
+
+rule2 <- function(x){
+  prediction <- rep(NA,length(x))
+  prediction[x > 2.8] <- "spam"
+  prediction[x <= 2.8] <- "nonspam"
+  return(prediction)
+}
+table(rule2(smallSpam$capitalAve),smallSpam$type)
+
+table(rule1(spam$capitalAve),spam$type)
+table(rule2(spam$capitalAve),spam$type)
+mean(rule1(spam$capitalAve)==spam$type)
+mean(rule2(spam$capitalAve)==spam$type)
