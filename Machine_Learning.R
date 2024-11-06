7075e7125615e547b72b636efcafd92cc4e2ea13
Patrick Niekamp <pniekamp113@gmail.com>
2024-11-06 08:05:53 -0500

    Update Machine_Learning.R
    updated

--- Machine_Learning.R
+++ Machine_Learning.R
@@ -577,3 +577,216 @@ model <- train(diagnosis ~ ., data = trainingIL, method = "glm")
 predict_model <- predict(model, newdata= testingIL)
 matrix_model <- confusionMatrix(predict_model, testingIL$diagnosis)
 matrix_model$overall[1]
+
+
+###
+#Module 3
+#Predicting with Trees
+
+# 1. Start with all variables in one group
+# 2. Find the variable/split that best seperates the outcomes
+# 3. Divide the data into two groups ("leaves") on that split ("node")
+# 4. Within each split, find the best variable/split that seperates the outcomes
+# 5. Continue until the groups are too small or insufficiently pure
+
+data(iris); library(ggplot2)
+names(iris)
+table(iris$Species)
+
+
+inTrain <- createDataPartition(y=iris$Species,
+                               p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training); dim(testing)
+
+qplot(Petal.Width,Sepal.Width,colour=Species,data=training)
+
+modFit <- train(Species ~ .,method="rpart",data=training)
+print(modFit$finalModel)
+
+plot(modFit$finalModel, uniform=TRUE, 
+     main="Classification Tree")
+text(modFit$finalModel, use.n=TRUE, all=TRUE, cex=.8)
+
+install.packages("rattle")
+library(rattle)
+fancyRpartPlot(modFit$finalModel)
+
+predict(modFit,newdata=testing)
+
+#Note that classification trees are non-linear models 
+
+# Bagging
+# Bootstrap aggregating
+# Resample cases and recalculate predictions 
+# Average or majority vote
+
+install.packages("mlbench")
+?ozone
+
+library(ElemStatLearn); data(ozone,package="ElemStatLearn")
+ozone <- ozone[order(ozone$ozone),]
+head(ozone)
+
+ll <- matrix(NA,nrow=10,ncol=155)
+for(i in 1:10){
+  ss <- sample(1:dim(ozone)[1],replace=T)
+  ozone0 <- ozone[ss,]; ozone0 <- ozone0[order(ozone0$ozone),]
+  loess0 <- loess(temperature ~ ozone,data=ozone0,span=0.2)
+  ll[i,] <- predict(loess0,newdata=data.frame(ozone=1:155))
+}
+
+plot(ozone$ozone,ozone$temperature,pch=19,cex=0.5)
+for(i in 1:10){lines(1:155,ll[i,],col="grey",lwd=2)}
+lines(1:155,apply(ll,2,mean),col="red",lwd=2)
+
+predictors = data.frame(ozone=ozone$ozone)
+temperature = ozone$temperature
+treebag <- bag(predictors, temperature, B = 10,
+               bagControl = bagControl(fit = ctreeBag$fit,
+                                       predict = ctreeBag$pred,
+                                       aggregate = ctreeBag$aggregate))
+
+
+plot(ozone$ozone,temperature,col='lightgrey',pch=19)
+points(ozone$ozone,predict(treebag$fits[[1]]$fit,predictors),pch=19,col="red")
+points(ozone$ozone,predict(treebag,predictors),pch=19,col="blue")
+
+library(caret)
+
+ctreeBag$fit
+ctreeBag$pred
+ctreeBag$aggregate
+
+
+### Random Forest
+
+data(iris); library(ggplot2)
+head(iris)
+library(randomForest)
+
+# 1. Boostrap samples
+# 2. At each split, boostrap variables
+# 3. Grow multiple trees and vote
+
+inTrain <- createDataPartition(y=iris$Species,
+                               p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+
+modFit <- train(Species~ .,data=training,method="rf",prox=TRUE) #method rf for random forest
+modFit
+getTree(modFit$finalModel,k=2)
+
+irisP <- classCenter(training[,c(3,4)], training$Species, modFit$finalModel$prox)
+irisP <- as.data.frame(irisP)
+irisP$Species <- rownames(irisP)
+p <- qplot(Petal.Width, Petal.Length, col=Species,data=training)
+p + geom_point(aes(x=Petal.Width,y=Petal.Length,col=Species),size=5,shape=4,data=irisP)
+
+pred <- predict(modFit,testing); testing$predRight <- pred==testing$Species
+table(pred,testing$Species)
+
+qplot(Petal.Width,Petal.Length,colour=predRight,data=testing,main="newdata Predictions")
+
+### Boosting
+
+# Take lots of possibly weak predictors and weigh them and add them up
+# Thereby getting stronger predictors
+
+library(ISLR); data(Wage); library(ggplot2); library(caret);
+Wage <- subset(Wage,select=-c(logwage))
+inTrain <- createDataPartition(y=Wage$wage,
+                               p=0.7, list=FALSE)
+training <- Wage[inTrain,]; testing <- Wage[-inTrain,]
+
+modFit <- train(wage ~ ., method="gbm",data=training,verbose=FALSE)
+print(modFit)
+
+qplot(predict(modFit,testing),wage,data=testing)
+
+###
+# Model based prediction
+
+data(iris); library(ggplot2)
+names(iris)
+table(iris$Species)
+
+inTrain <- createDataPartition(y=iris$Species,
+                               p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training); dim(testing)
+
+modlda = train(Species ~ .,data=training,method="lda")
+modnb = train(Species ~ ., data=training,method="nb")
+plda = predict(modlda,testing); pnb = predict(modnb,testing)
+table(plda,pnb)        
+
+equalPredictions = (plda==pnb)
+qplot(Petal.Width,Sepal.Width,colour=equalPredictions,data=testing)
+
+####
+#Quiz
+
+# Question 1
+
+library(AppliedPredictiveModeling)
+data(segmentationOriginal)
+library(caret)
+
+head(segmentationOriginal)
+dim(segmentationOriginal)
+
+inTrain <- createDataPartition(y=segmentationOriginal$Case,
+                               p=0.6, list=FALSE)
+
+training <- segmentationOriginal[inTrain,]
+testing <- segmentationOriginal[-inTrain,]
+dim(training); dim(testing)
+
+
+set.seed(125)
+
+modFit <- train(Case ~ .,method="rpart",data=training)
+modFit
+modFit$finalModel
+
+# Question 3
+
+install.packages("pgmm")
+library(pgmm)
+data(olive)
+olive = olive[,-1]
+head(olive)
+unique(olive$Area)
+
+inTrain <- createDataPartition(y=olive$Area,
+                               p=0.7, list=FALSE)
+training <- olive[inTrain,]
+testing <- olive[-inTrain,]
+dim(training); dim(testing)
+
+modFit <- train(Area~ .,data=training,method="rf",prox=TRUE) #method rf for random forest
+modFit
+getTree(modFit$finalModel,k=2)
+
+
+oliveP <- classCenter(training[,c(3,4)], training$Area, modFit$finalModel$prox)
+#irisP <- as.data.frame(irisP)
+newdata = as.data.frame(t(colMeans(olive)))
+
+olive$Area <- rownames(newdata)
+head(olive)
+head(newdata)
+
+#p <- qplot(Palmitic, Palmitoleic, col=Area,data=training)
+#p + geom_point(aes(x=Palmitic,y=Palmitoleic,col=Area),size=5,shape=4,data=olive)
+
+pred <- predict(modFit,testing)
+#testing$predRight <- pred==testing$Species
+#table(pred,testing$Species)
+
+#qplot(Petal.Width,Petal.Length,colour=predRight,data=testing,main="newdata Predictions")
+
